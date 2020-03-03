430312fb0794d0d622a1e58feb214a6bc68cd736
Kef <theonlykef@gmail.com>
2020-03-03 22:03:25 +0200

    Some cleanup

--- rofi/RFAppListController.m
+++ rofi/RFAppListController.m
@@ -13,10 +13,8 @@ NSMutableArray *selectedApps;
 	self = [super initWithStyle:style];
 	selectedApps = [[NSMutableArray alloc] init];
 	SparkAppList *appList = [[SparkAppList alloc] init];
-	//NSLog(@"[RF] viewDidLoad called, self = %p", self);
 	__weak RFAppListController *weakSelf = self;
 	[appList getAppList:^(NSArray *args) {
-		//NSLog(@"[RF] block called. args length = %lu", [args count]);
 		userApps = [args mutableCopy];
 		NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"displayName" ascending:YES];
 		[userApps sortUsingDescriptors:@[sortDescriptor]];
@@ -29,60 +27,10 @@ NSMutableArray *selectedApps;
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	//[self setEditingButtonHidden:YES animated:NO];
-	//[self setEditable:YES];
 	[self setEditing:YES animated:NO];
 	self.navigationItem.hidesBackButton = NO;
 	self.title = @"Selected Applications";
 }
-
-/*
-- (NSArray *)specifiers {
-	NSLog(@"[RF] specifiers called");
-	appList = [ALApplicationList sharedApplicationList];
-	NSMutableArray *result = [[NSMutableArray alloc] init];
-	//NSArray *sortedDisplayIdentifiers;
-	NSArray *testArray;
-	applications = [[appList applicationsFilteredUsingPredicate:[NSPredicate predicateWithFormat:@"isSystemApplication = FALSE"] onlyVisible:YES titleSortedIdentifiers:&testArray] mutableCopy];
-	sortedDisplayIdentifiers = [testArray mutableCopy];
-	PSSpecifier *specifier0 = [PSSpecifier preferenceSpecifierNamed:@"Added apps"
-                                                            target:self
-                                                               set:nil
-                                                               get:nil
-                                                            detail:Nil
-                                                              cell:PSGroupCell
-                                                              edit:Nil];
-	[result addObject:specifier0];
-	firstSection = specifier0;
-	PSSpecifier *specifier1 = [PSSpecifier preferenceSpecifierNamed:@"System apps"
-                                                            target:self
-                                                               set:nil
-                                                               get:nil
-                                                            detail:Nil
-                                                              cell:PSGroupCell
-                                                              edit:Nil];
-	[result addObject:specifier1];
-	for (NSString *appId in sortedDisplayIdentifiers) {
-		UIImage *icon = [appList iconOfSize:ALApplicationIconSizeSmall forDisplayIdentifier:appId];
-		PSSpecifier *specifier = [PSSpecifier preferenceSpecifierNamed:applications[appId]
-                                                                target:self
-                                                                   set:@selector(setPreferenceValue:specifier:)
-                                                                   get:@selector(readPreferenceValue:)
-                                                                detail:Nil
-                                                                  cell:PSListItemCell
-                                                                  edit:Nil];
-		[specifier setIdentifier:appId];
-		[specifier setProperty:@YES forKey:@"enabled"];
-		[specifier setProperty:icon forKey:@"iconImage"];
-		//NSLog(@"[RF] specifier = %@", specifier);
-		[result addObject:specifier];
-
-	}
-	_specifiers = [result copy];
-	//NSLog(@"[RF] _specifiers = %@", _specifiers);
-	return _specifiers;
-}
-*/
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 2;
@@ -180,34 +128,4 @@ NSMutableArray *selectedApps;
 		[tableView endUpdates];
 	}
 }
-/*
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-	if (editingStyle == UITableViewCellEditingStyleInsert) {
-		NSInteger ind = indexPath.row + [tableView numberOfRowsInSection:0] + 3;
-		NSLog(@"[RF] numberOfRowsInSection = %ld, row = %ld, ind = %ld", [tableView numberOfRowsInSection:0], indexPath.row, ind);
-		//[tableView moveRowAtIndexPath:indexPath toIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
-		//[self insertSpecifier:_specifiers[indexPath.row + [tableView numberOfRowsInSection:0] + 2] atIndex:0 animated:YES];
-		UIImage *icon = [appList iconOfSize:ALApplicationIconSizeSmall forDisplayIdentifier:sortedDisplayIdentifiers[indexPath.row]];
-		PSSpecifier *specifier = [PSSpecifier preferenceSpecifierNamed:applications[sortedDisplayIdentifiers[indexPath.row]]
-	                                                            target:self
-	                                                               set:nil
-	                                                               get:nil
-	                                                            detail:Nil
-	                                                              cell:PSListItemCell
-	                                                              edit:Nil];
-		[specifier setProperty:@YES forKey:@"enabled"];
-		[specifier setProperty:icon forKey:@"iconImage"];
-		//[self insertSpecifier:specifier afterSpecifier:firstSection animated:YES];
-		[self insertSpecifier:specifier atIndex:1 animated:YES];
-		[self removeSpecifierAtIndex:ind animated:YES];
-		[applications removeObjectForKey:sortedDisplayIdentifiers[indexPath.row]];
-		[sortedDisplayIdentifiers removeObjectAtIndex:indexPath.row];
-	}
-}
-*/
-/*
--(id)_editButtonBarItem {
-	return nil;
-}
-*/
 @end
